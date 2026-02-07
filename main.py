9a9d5748eb23face92fd5134e6ae739f0100c0a4
sreesha variseri <svariseri@ufl.edu>
2026-02-07 15:13:32 -0500

    yyay

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import test
 
 print("make an account")
 _name=input("Name: ")
-_age=input("Age: ")
+_age=int(input("Age: "))
 _sex=input("Sex: ")
-_height=int(input("Height: "))
-_weight=int(input("Weight: "))
-_maingoal=int(input("Main Goal: "))
-_exceriselvl=int(input("1.2 is for little or no exercise\n 1.4 is for light exercise 1-2 times a week\n 1.6 is for moderate exercise 2-3 times/week\n 1.75 is for hard exercise 3-5 times/week\n2.0 if you've got a physical job or perform hard exercise 6-7 times/week "))
+_height=float(input("Height: "))
+_weight=float(input("Weight: "))
+_maingoal=float(input("Main Goal: "))
+_exceriselvl=float(input("1.2 is for little or no exercise\n 1.4 is for light exercise 1-2 times a week\n 1.6 is for moderate exercise 2-3 times/week\n 1.75 is for hard exercise 3-5 times/week\n2.0 if you've got a physical job or perform hard exercise 6-7 times/week "))
 _name=test.Acount(_name,_age,_sex,_height,_weight,_maingoal,_exceriselvl)
 
 
@@ -21,5 +21,9 @@ while True:
     if m=="1":
         c=int(input("Adding:"))
         _name.addcal(c)
+    elif m=="2":
+        print("Main Goal is to reach",_name.getgoal())
+    elif m=="3":
+        print("Total Calories:", _name.getcal())
 
 
